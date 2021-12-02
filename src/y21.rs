df7561e456bef243b08abbc7dca3843d150db886
Jan Reggie Dela Cruz <janreggie.delacruz@gmail.com>
2021-12-02 19:31:59 +0800

    y21-d02: Dive!

--- src/y21.rs
+++ src/y21.rs
@@ -1,8 +1,10 @@
 mod d01;
+mod d02;
 
 pub fn solver(day: u8) -> fn(&Vec<String>) -> Result<(String, String), String> {
     match day {
         1 => d01::d01,
+        2 => d02::d02,
         _ => panic!("unimplemented day {}", day),
     }
 }
---
+++ src/y21/d02.rs
@@ -0,0 +1,82 @@
+/// A submarine instruction.
+enum Instruction {
+    Forward(u32),
+    Down(u32),
+    Up(u32),
+}
+
+impl Instruction {
+    /// Create insturction from line such as `forward 5` or `down 4`
+    fn new(line: &str) -> Result<Instruction, String> {
+        let split: Vec<&str> = line.split(' ').collect();
+        if split.len() != 2 {
+            return Err(format!(
+                "expected line to be split into two parts, got {}",
+                split.len()
+            ));
+        }
+
+        let count = split[1];
+        let count: Result<u32, _> = count.parse();
+        if let Err(_) = count {
+            return Err(format!("could not format count `{}` as u32", split[1]));
+        }
+        let count = count.unwrap();
+
+        match split[0] {
+            "forward" => Ok(Instruction::Forward(count)),
+            "down" => Ok(Instruction::Down(count)),
+            "up" => Ok(Instruction::Up(count)),
+            _ => Err(format!(
+                "could not interpret instruction `{}` properly",
+                split[0]
+            )),
+        }
+    }
+}
+
+pub fn d02(lines: &Vec<String>) -> Result<(String, String), String> {
+    let mut instrs = Vec::new();
+    for line in lines {
+        let instr = Instruction::new(line);
+        match instr {
+            Ok(instr) => instrs.push(instr),
+            Err(e) => {
+                return Err(format!(
+                    "could not parse instruction of line `{}`: `{}`",
+                    line, e
+                ))
+            }
+        }
+    }
+
+    // Part 1: A simpler way to go through all instructions
+    let mut horizontal: u32 = 0;
+    let mut vertical: u32 = 0;
+    for instr in &instrs {
+        match instr {
+            Instruction::Forward(c) => horizontal += c,
+            Instruction::Down(c) => vertical += c,
+            Instruction::Up(c) => vertical -= c,
+        }
+    }
+    let ans1 = horizontal * vertical;
+
+    // Part 2: Something about "aim"
+    let mut horizontal: u32 = 0;
+    let mut vertical: u32 = 0;
+    let mut aim: u32 = 0;
+    for instr in &instrs {
+        match instr {
+            Instruction::Forward(c) => {
+                horizontal += c;
+                vertical += aim * c;
+            }
+            Instruction::Down(c) => aim += c,
+            Instruction::Up(c) => aim -= c,
+        }
+    }
+    let ans2 = horizontal * vertical;
+
+    Ok((ans1.to_string(), ans2.to_string()))
+}
