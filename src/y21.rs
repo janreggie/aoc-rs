8c57b6932cb3304385b4af9e01ad0f05ca402be7
Jan Reggie Dela Cruz <janreggie.delacruz@gmail.com>
2022-01-03 22:46:28 +0800

    removed y21-d24

--- src/y21.rs
+++ src/y21.rs
@@ -20,7 +20,6 @@ mod d17;
 mod d20;
 mod d21;
 mod d22;
-mod d24;
 
 pub fn solver(day: u8) -> fn(Vec<String>) -> Result<(String, String)> {
     match day {
@@ -44,7 +43,6 @@ pub fn solver(day: u8) -> fn(Vec<String>) -> Result<(String, String)> {
         20 => d20::solve,
         21 => d21::solve,
         22 => d22::solve,
-        24 => d24::solve,
         _ => todo!("day {}", day),
     }
 }
