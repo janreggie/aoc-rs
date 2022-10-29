3b0b1893887de11ccc4c2a28a3e0eee96d0542e6
Jan Reggie Dela Cruz <janreggie.delacruz@gmail.com>
2022-10-29 13:53:12 +0800

    y17-d05 optimize

--- src/y17/d05.rs
+++ src/y17/d05.rs
@@ -1,44 +1,33 @@
 use crate::util::vectors;
-use anyhow::{bail, Context, Result};
+use anyhow::{Context, Result};
 
 pub fn solve(lines: Vec<String>) -> Result<(String, String)> {
     let instructions_master: Vec<i32> =
         vectors::from_strs(&lines).context("could not convert instructions to numbers")?;
-    let instructions_len = instructions_master.len() as i32;
 
     // Part 1: Increment by 1 for each instruction
     let mut instructions = instructions_master.clone();
-    let mut step_count = 1;
+    let mut step_count = 0;
     let mut ind = 0;
-    loop {
-        let current = instructions[ind];
-        let next_ind = current + (ind as i32);
-        if next_ind < 0 || next_ind >= instructions_len {
-            break;
-        }
+    while let Some(current) = instructions.get_mut(ind as usize) {
+        ind += *current;
         step_count += 1;
-        instructions[ind] += 1;
-        ind = next_ind as usize;
+        *current += 1;
     }
     let ans1 = step_count.to_string();
 
     // Part 2: Inc by 1 if <3, dec by 1 otherwise
     let mut instructions = instructions_master.clone();
-    let mut step_count = 1;
+    let mut step_count = 0;
     let mut ind = 0;
-    loop {
-        let current = instructions[ind];
-        let next_ind = current + (ind as i32);
-        if next_ind < 0 || next_ind >= instructions_len {
-            break;
-        }
+    while let Some(current) = instructions.get_mut(ind as usize) {
+        ind += *current;
         step_count += 1;
-        if current >= 3 {
-            instructions[ind] -= 1;
+        if *current >= 3 {
+            *current -= 1;
         } else {
-            instructions[ind] += 1;
+            *current += 1;
         }
-        ind = next_ind as usize;
     }
     let ans2 = step_count.to_string();
 
