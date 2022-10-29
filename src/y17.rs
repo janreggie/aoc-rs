1f153796b73c7a26fb3cddebe2adbf35e27074db
Jan Reggie Dela Cruz <janreggie.delacruz@gmail.com>
2022-10-29 13:20:23 +0800

    y17-d04

--- src/y17.rs
+++ src/y17.rs
@@ -3,12 +3,14 @@ use anyhow::Result;
 mod d01;
 mod d02;
 mod d03;
+mod d04;
 
 pub fn solver(day: u8) -> fn(Vec<String>) -> Result<(String, String)> {
     match day {
         1 => d01::solve,
         2 => d02::solve,
         3 => d03::solve,
+        4 => d04::solve,
         _ => todo!("day {}", day),
     }
 }
---
+++ src/y17/d04.rs
@@ -0,0 +1,43 @@
+use crate::util::vectors;
+use anyhow::Result;
+
+/// Sorts a string by chars ("acdb" -> "abcd")
+fn sort_string(s: &String) -> String {
+    let mut chars: Vec<char> = s.chars().collect();
+    chars.sort();
+    String::from_iter(chars)
+}
+
+pub fn solve(lines: Vec<String>) -> Result<(String, String)> {
+    let passphrases = lines;
+
+    // Number of valid passphrases for parts 1 and 2 respectively
+    let mut valid_passphrases_1 = 0;
+    let mut valid_passphrases_2 = 0;
+
+    for passphrase in passphrases {
+        let mut split_passphrase = vectors::split_and_trim(&passphrase, ' ');
+        let initial_length = split_passphrase.len();
+
+        // Part 1: Check if the passphrase uses duplicate words
+        split_passphrase.sort();
+        split_passphrase.dedup();
+        let final_length = split_passphrase.len();
+        if initial_length == final_length {
+            valid_passphrases_1 += 1;
+        }
+
+        // Part 2: Check if the passphrase uses anagrams of each other
+        split_passphrase = split_passphrase.iter().map(sort_string).collect();
+        split_passphrase.sort();
+        split_passphrase.dedup();
+        let final_length = split_passphrase.len();
+        if initial_length == final_length {
+            valid_passphrases_2 += 1;
+        }
+    }
+
+    let ans1 = valid_passphrases_1.to_string();
+    let ans2 = valid_passphrases_2.to_string();
+    Ok((ans1, ans2))
+}
