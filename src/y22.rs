e183530ff8e491494a6ffa0428f4307c30c0b0b9
Jan Reggie Dela Cruz <janreggie.delacruz@gmail.com>
2022-12-08 15:55:31 +0800

    y22-d06

--- src/y22.rs
+++ src/y22.rs
@@ -5,6 +5,7 @@ mod d02;
 mod d03;
 mod d04;
 mod d05;
+mod d06;
 
 pub fn solver(day: u8) -> fn(Vec<String>) -> Result<(String, String)> {
     match day {
@@ -13,6 +14,7 @@ pub fn solver(day: u8) -> fn(Vec<String>) -> Result<(String, String)> {
         3 => d03::solve,
         4 => d04::solve,
         5 => d05::solve,
+        6 => d06::solve,
         _ => todo!("day {}", day),
     }
 }
---
+++ src/y22/d06.rs
@@ -0,0 +1,81 @@
+use std::collections::HashMap;
+
+use anyhow::{bail, Context, Result};
+
+fn byte_to_usize(b: u8) -> usize {
+    (b - b'a').into()
+}
+
+/// Returns ind such that bytes[ind-length:ind] contains unique characters, if such ind exists.
+/// If bytes is shorter than length, return None.
+/// It is assumed that bytes contains lowercase letters 'a' to 'z'.
+fn get_unique_span(bytes: &[u8], length: usize) -> Option<usize> {
+    if bytes.len() < length {
+        return None;
+    }
+
+    // Initial population
+    let mut marked_bytes = [false; 26];
+    let mut duplicate_bytes: HashMap<u8, i32> = HashMap::new(); // number of excess times byte appears
+    for ii in 0..length {
+        let b = bytes[ii];
+        if marked_bytes[byte_to_usize(b)] {
+            duplicate_bytes
+                .entry(b)
+                .and_modify(|c| *c += 1)
+                .or_insert(1);
+        }
+        marked_bytes[byte_to_usize(b)] = true;
+    }
+
+    // Now, span
+    for ii in length..bytes.len() {
+        let (prev_byte, next_byte) = (bytes[ii - length], bytes[ii]);
+        // First, either remove prev_byte
+        if duplicate_bytes.contains_key(&prev_byte) {
+            duplicate_bytes.entry(prev_byte).and_modify(|c| *c -= 1);
+            if *duplicate_bytes.get(&prev_byte).unwrap_or(&0) == 0 {
+                duplicate_bytes.remove(&prev_byte);
+            }
+        } else {
+            marked_bytes[byte_to_usize(prev_byte)] = false;
+        }
+
+        // Then, insert next_byte
+        if marked_bytes[byte_to_usize(next_byte)] {
+            duplicate_bytes
+                .entry(next_byte)
+                .and_modify(|c| *c += 1)
+                .or_insert(1);
+        } else {
+            marked_bytes[byte_to_usize(next_byte)] = true;
+        }
+
+        // Finally, check if there are no longer any duplicate bytes
+        if duplicate_bytes.len() == 0 {
+            return Some(ii + 1); // Explain why the +1?!
+        }
+    }
+
+    None
+}
+
+pub fn solve(lines: Vec<String>) -> Result<(String, String)> {
+    if lines.len() != 1 {
+        bail!("expected input to be of length 1, got {}", lines.len())
+    }
+    let input = lines.into_iter().next().unwrap();
+    let input = input.as_bytes();
+    if input.len() < 14 {
+        bail!("input too short")
+    }
+
+    let ans1 = get_unique_span(input, 4)
+        .context("could not get start of packet marker")?
+        .to_string();
+    let ans2 = get_unique_span(input, 14)
+        .context("could not get start of message marker")?
+        .to_string();
+
+    Ok((ans1, ans2))
+}
